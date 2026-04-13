3efbecf6474825dde0934f5a3bcb767f28a22cea
Trishit Sanyal <trishitsanyal23@gmail.com>
2026-04-13 10:13:47 +0530

    UC7: String.join() to concatenate multiple names with delimiter

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -3,12 +3,8 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String greeting = "";
-            for (String name : args) {
-                greeting += name + ", ";
-            }
-            greeting = greeting.substring(0, greeting.length() - 2);
-            System.out.println("Hello, " + greeting + "!");
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
